f7e67151e14615558933aaee625cae564c653ae2
Petr Panteleyev <petr@panteleyev.org>
2021-09-11 23:56:29 +0300

    currency dialog validation a bit

--- frontend/src/app/dialogs/currency-dialog/currency-dialog.component.ts
+++ frontend/src/app/dialogs/currency-dialog/currency-dialog.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {Currency} from "../../model/currency";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Store} from "@ngrx/store";
 import {createCurrencyAction, updateCurrencyAction} from "../../state/data-actions";
 
@@ -30,6 +30,10 @@ export class CurrencyDialogComponent implements OnInit {
   }
 
   onOk() {
+    if (this._symbol?.hasError('required')) {
+      return
+    }
+
     let value = this.form.value
 
     if (this.currency == undefined) {
@@ -74,5 +78,9 @@ export class CurrencyDialogComponent implements OnInit {
       useThousandSeparator: [this.currency?.useThousandSeparator || false, []],
       rate: [this.currency?.rate || 1, []]
     });
+  }
+
+  get _symbol() {
+    return this.form.get("symbol")
   }
 }
